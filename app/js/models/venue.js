02ad7b7e474253ed4ddc1d6ea59fcab6984fc0f8
Tommy Barth <tommyhbrc@gmail.com>
2014-10-27 15:20:35 -0400

    fixes async and builds

--- app/js/models/venue.js
+++ app/js/models/venue.js
@@ -3,5 +3,5 @@ Venue.Venue = DS.Model.extend({
   venueAddress: DS.attr('string'),
   venueDescription: DS.attr('string'),
   venueImgUrl: DS.attr('string'),
-  venueBands: DS.hasMany('band')
+  venueBands: DS.hasMany('band', {async: true})
 });
