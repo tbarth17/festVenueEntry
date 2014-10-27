0c8500fde143c2dbf52604fc6922ec6e34ff9cdc
Tommy Barth <tommyhbrc@gmail.com>
2014-10-27 12:18:44 -0400

    Adds date functionality

--- app/js/controllers/band_controller.js
+++ app/js/controllers/band_controller.js
@@ -7,8 +7,10 @@ Venue.BandController = Ember.Controller.extend({
       var band = this.store.createRecord('band', {
         bandName: this.get("bandName"),
         bandImgUrl: this.get("bandImgUrl"),
+        bandEmbedUrl: this.get("bandEmbedUrl"),
         bandBio: this.get('bandBio'),
-        bandSetTime: this.get("bandSetTime"),
+        bandStartTime: new Date(this.get("bandStartTime")),
+        bandEndTime: new Date(this.get("bandEndTime"))
       });
       band.set('bandVenue', bandVenue);
       // var serializer = this.store.serializerFor('band');
--- app/js/models/band.js
+++ app/js/models/band.js
@@ -2,6 +2,8 @@ Venue.Band = DS.Model.extend({
   bandName: DS.attr('string'),
   bandBio: DS.attr('string'),
   bandImgUrl: DS.attr('string'),
-  bandSetTime: DS.attr('string'),
+  bandStartTime: DS.attr('date'),
+  bandEndTime: DS.attr('date'),
+  bandEmbedUrl: DS.attr('string'),
   bandVenue: DS.belongsTo('venue')
 });
