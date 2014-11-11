98bc6aa8f58c1a8a53c5c9ed92261edc65db49d4
Tommy Barth <tommyhbrc@gmail.com>
2014-11-11 11:50:19 -0500

    Changes firebase

--- app/js/application.js
+++ app/js/application.js
@@ -2,7 +2,7 @@
 	'use strict';
 	window.Venue = Ember.Application.create();
 
-  Venue.fireRef = new Firebase("https://premature-optimization.firebaseio.com/");
+  Venue.fireRef = new Firebase("https://fest2.firebaseio.com/");
 
 	Venue.ApplicationAdapter = DS.FirebaseAdapter.extend({
 	  firebase: Venue.fireRef
